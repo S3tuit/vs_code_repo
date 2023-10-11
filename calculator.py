6610e473555ab4e194515600a649a947db30c6f5
Matteo Ladislai <matteo.ladislai@hotmail.com>
2023-10-11 22:53:33 +0200

    calculator v0.9

--- calculator.py
+++ calculator.py
@@ -23,6 +23,65 @@ class Num_button(Button):
         new_text = str(current_text) + str(self.num)
         label_top.config(text=new_text)
 
+class Func_button(Button):
+
+    def __init__ (self, sign, row_n, col_n):
+        super().__init__(window,
+                         bg="silver",
+                         fg="black",
+                         text=sign,
+                         command=self.func)
+        self.sign = sign
+        self.grid(row=row_n,
+                  column=col_n,
+                  sticky="ew")
+        
+    def func(self):
+        global label_top
+        global last_func
+        global current_num
+
+        try:
+            last_func = self.sign
+            current_num = int(label_top.cget("text"))
+            label_top.config(text="")
+        except ValueError:
+            pass
+
+class Subm_button(Button):
+
+    def __init__ (self, row_n, col_n):
+        super().__init__(window,
+                         bg="silver",
+                         fg="black",
+                         text="=",
+                         command=self.subm)
+        self.grid(row=row_n,
+                  column=col_n,
+                  sticky="ew")
+        
+    def subm(self):
+        global label_top
+        global last_func
+        global current_num
+
+        try:
+            if last_func == "+":
+                new_nums = int(label_top.cget("text")) + current_num
+                label_top.config(text=new_nums)
+                current_num = 0
+                last_func = ""
+            elif last_func == "-":
+                new_nums = current_num - int(label_top.cget("text"))
+                label_top.config(text=new_nums)
+                current_num = 0
+                last_func = ""
+            elif last_func == "":
+                pass
+        except ValueError:
+            pass
+
+
 def label_top_f():
     global label_top
     label_top = Label(window,
@@ -33,8 +92,24 @@ def label_top_f():
                       font=("Arial", 27, "bold"))
     label_top.grid(row=0, column=0, sticky="ew", columnspan=5)
 
+
+def button_del():
+    global label_top
+    current_text = label_top.cget("text")
+    label_top.config(text=current_text[0:-1])
+
+def button_ac():
+    global label_top
+    global current_num
+    global last_func
+    last_func = ""
+    current_num = 0
+    label_top.config(text="")
+
 window = Tk()
 window.geometry("500x700")
+last_func = ""
+current_num = 0
 
 window.grid_columnconfigure(0, weight=10)
 window.grid_columnconfigure(1, weight=10)
@@ -52,5 +127,29 @@ num6 = Num_button("6", 2, 2)
 num7 = Num_button("7", 3, 0)
 num8 = Num_button("8", 3, 1)
 num9 = Num_button("9", 3, 2)
+num0 = Num_button("0", 4, 0)
+num_add = Func_button("+", 1, 4)
+num_add = Func_button("-", 2, 4)
+num_subm = Subm_button(3, 4)
+
+num_del = Button(window,
+                bg="silver",
+                fg="black",
+                text="Del",
+                command=button_del
+                )
+num_del.grid(row=4,
+            column=4,
+            sticky="ew")
+
+num_ac = Button(window,
+                bg="silver",
+                fg="black",
+                text="AC",
+                command=button_ac
+                )
+num_ac.grid(row=4,
+            column=2,
+            sticky="ew")
 
 window.mainloop()
