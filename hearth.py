d1532a69b031b6858c1ae655c5a2f7e13946adc3
Matteo Ladislai <matteo.ladislai@hotmail.com>
2023-10-17 21:47:42 +0200

    statistica migl. mediana

--- hearth.py
+++ hearth.py
@@ -1,48 +1,21 @@
-import tkinter as tk
-import time
+import numpy as np
 
-def move_circle():
-    while True:
-        canvas.move(circle,Xv,Yv)
-        root.update()
-        time.sleep(0.03)
-        x1, y1, x2, y2 = canvas.coords(circle)
-        center_x = (x1 + x2) / 2
-        center_y = (y1 + y2) / 2
-        move_curve(center_x, center_y, x2, y2)
+numbers = [26, 28, 29, 29, 30, 32, 32, 33, 35, 36, 36, 37,37,37,39,39,42,42,43,47,48]
 
-def move_curve(x1, y1, x2, y2):
-    global points
-    root.update()
-    points.append(x1)
-    points.append(y1)
-    points.append(x1)
-    points.append(y1)
-    root.update()
-    circle = canvas.create_line(points, smooth = True, fill="red")
+# Sort the data
+sorted_numbers = sorted(numbers)
+
+# Calculate Q1 (25th percentile)
+q1 = np.percentile(sorted_numbers, 25)
+
+# Calculate Q3 (75th percentile)
+q2 = np.percentile(sorted_numbers, 50)
+
+q3 = np.percentile(sorted_numbers, 75)
+
+print(f"Q1: {q1}")
+print(f"Q2: {q2}")
+print(f"Q3: {q3}")
 
 
-points = []
 
-Xv=3
-Yv=-3
-
-root = tk.Tk()
-
-canvas = tk.Canvas(root,
-                   height=570,
-                   width=570,
-                   bg="black")
-canvas.pack()
-
-curve = canvas.create_line(290, 290,
-                            300, 300,
-                            smooth = True, fill="red")
-
-circle = canvas.create_oval(290, 290,
-                            300, 300,
-                            fill="white")
-
-move_circle()
-
-root.mainloop()
--- statistic.py
+++ statistic.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # tunrs a string of things into a list of only numbers
 # ex: input:"23uiw -2--..4" --> output:[23, 2, 4].
 def turn_list(num):
@@ -65,13 +67,10 @@ def calc_cum_freq(Fj_dict_copy):
 # input should be list of numbers
 def calc_avg(nums_copy):
     nums = nums_copy.copy()
-    count = 0
-    sum = 0
-    for i in nums:
-        count += 1
-        sum += i
-    result = round(sum / count, 2)
-    return result
+    total_sum = sum(numbers)
+    total_count = len(numbers)
+    avg = total_sum / total_count
+    return avg
 
 # input should be abs_freq
 # output is a list with the different moda
@@ -85,59 +84,101 @@ def calc_moda(nums_copy):
 
 # input should be list of numbers
 def calc_median(nums_copy):
-    length = len(nums_copy)
+    nums = nums_copy.copy()
+    length = len(nums)
+    nums.sort()
     if length % 2 == 0:
-        n1 = nums_copy[int(length / 2)]
-        n2 = nums_copy[int((length / 2) + 1)]
+        n1 = nums[length // 2-1]
+        n2 = nums[length // 2]
         median = (n1 + n2) / 2
     else:
-        median = nums_copy[int((length + 1) / 2)]
+        median = nums[length // 2]
     return median
 
 # input should be list of numbers
 def calc_quartili(nums_copy):
-    q2 = calc_median(nums_copy)
-    length = len(nums_copy)
-    if q2 in nums_copy:
-        nums_1 = nums_copy[:int((length-1) / 2):]
+    nums = nums_copy.copy()
+    length = len(nums)
+    nums.sort()
+    q2 = calc_median(nums)
+    if length % 2 == 0:
+        nums_1 = nums[:length // 2]
         q1 = calc_median(nums_1)
-        nums_3 = nums_copy[int((length-1) / 2) ::]
+        nums_3 = nums[length // 2:]
         q3 = calc_median(nums_3)
     else:
-        nums_1 = nums_copy[:int(length / 2):]
+        nums_1 = nums[:(length // 2)+1]
         q1 = calc_median(nums_1)
-        nums_3 = nums_copy[int(length / 2) ::]
+        nums_3 = nums[length // 2:]
         q3 = calc_median(nums_3)
     quartili = {}
-    quartili["Q0"] = nums_copy[0]
+    quartili["Q0"] = nums[0]
     quartili["Q1"] = q1
     quartili["Q2"] = q2
     quartili["Q3"] = q3
-    quartili["Q4"] = nums_copy[-1]
+    quartili["Q4"] = nums[-1]
     return quartili
-    
 
-numbers = "32 65 45 42. 67 8 6 ---- 65bugjy32 23 32 23 43  43 23 43 43 23"
-carattere = "Età"
+# input should be list of numbers
+# return coeficciente di variazione, varianza, scarto_q_medio
+def calc_variabilita(nums_copy):
+    nums = nums_copy.copy()
+    media = calc_avg(nums)
+    devianza = 0
+    for i in nums:
+        valore = (i - media)**2
+        devianza += valore
+    varianza = devianza / len(nums)
+    scarto_q_medio = sqrt(varianza)
+    cv = (scarto_q_medio / media) * 100
+    return cv, varianza, scarto_q_medio
 
-caratteri = turn_list(numbers)
+def calc_scost_mediana(nums_copy):
+    nums = nums_copy.copy()
+    mediana = calc_median(nums)
+    sum = 0
+    for i in nums:
+        valore = abs(i - mediana)
+        sum += i
+    scost_mediana = sum / len(nums)
+    return scost_mediana
 
-abs_freq = calc_abs_freq(caratteri)
+# return indice_fisher
+def calc_asimmetria(nums_copy):
+    nums = nums_copy.copy()
+    media = calc_avg(nums)
+    sum = 0
+    for i in nums:
+        valore = (i - media)**3
+        sum += valore
+    indice_asimm = sum / len(nums)
+    result = calc_variabilita(nums_copy)
+    scarto_q_medio = result[2]
+    indice_fisher = indice_asimm / (scarto_q_medio**3)
+    return indice_fisher
 
-rel_freq = calc_rel_freq(abs_freq)
 
-perc_freq = calc_perc_freq(rel_freq)
 
-cum_freq = calc_cum_freq(rel_freq)
+numbers = [26, 28, 29, 29, 30, 32, 32, 33, 35, 36, 36, 37,37,37,39,39,42,42,43,47,48]
+#numbers = [0,2,6,8,11,15,17,17,18,18,19,19,20,23,23,23,24,26,27,27,28]
 
+cv, varianza, scarto_q_medio = calc_variabilita(numbers)
+
+fisher = calc_asimmetria(numbers)
+
+media = calc_avg(numbers)
+mediana = calc_median(numbers)
+abs_freq = calc_abs_freq(numbers)
 moda = calc_moda(abs_freq)
 
-quartili = calc_quartili(caratteri)
+quart = calc_quartili(numbers)
 
-median = calc_median(caratteri)
+print("————————————————————————")
+print(quart)
+print()
 
+print(f"Mediana: {mediana}\nMedia: {media}\nModa: {moda}\n")
 
-print(caratteri)
-print(abs_freq)
-print(moda)
-print(quartili)
+print(f"Coeff. var.: {cv}\nVarianza: {varianza}\nScarto Q Medio: {scarto_q_medio}")
+print(f"Indice di Fisher= {fisher}")
+print("————————————————————————")
