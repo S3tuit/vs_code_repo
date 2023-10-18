506389bf707d14811f2fdc6abc797d95526c0f51
Matteo Ladislai <matteo.ladislai@hotmail.com>
2023-10-18 11:29:59 +0200

    BoxPlot v1

--- statistic.py
+++ statistic.py
@@ -45,7 +45,7 @@ def calc_rel_freq(nj_dict):
     for v in nj_dict.values():
         N += v
     for k, v in nj_dict.items():
-        fj_dict[k] = round((v / N), 2)
+        fj_dict[k] = round((v / N), 3)
     return fj_dict
 
 # input should be rel_freq
@@ -61,16 +61,21 @@ def calc_cum_freq(Fj_dict_copy):
     value = 0
     for k, v in Fj_dict.items():
         value += v
-        Fj_dict[k] = value
+        Fj_dict[k] = round(value, 3)
     return Fj_dict
 
-# input should be list of numbers
+# input should be numbers or abs freq.
 def calc_avg(nums_copy):
-    nums = nums_copy.copy()
-    total_sum = sum(numbers)
-    total_count = len(numbers)
-    avg = total_sum / total_count
-    return avg
+    if type(nums_copy) == list:
+        nums = nums_copy.copy()
+        total_sum = sum(numbers)
+        total_count = len(numbers)
+        avg = total_sum / total_count
+        return avg
+    elif type(nums_copy) == dict:
+        total = sum(value * frequency for value, frequency in nums_copy.items())
+        length = sum(nums_copy.values())
+        return total / length
 
 # input should be abs_freq
 # output is a list with the different moda
@@ -82,103 +87,133 @@ def calc_moda(nums_copy):
     moda = {key: value for (key, value) in nums.items() if value == max_value}
     return moda
 
-# input should be list of numbers
-def calc_median(nums_copy):
+# input should be list of numbers or cum freq.
+def calc_median(nums_copy, q):
     nums = nums_copy.copy()
-    length = len(nums)
-    nums.sort()
-    if length % 2 == 0:
-        n1 = nums[length // 2-1]
-        n2 = nums[length // 2]
-        median = (n1 + n2) / 2
-    else:
-        median = nums[length // 2]
-    return median
+    if isinstance(nums, list):
+        length = len(nums)
+        nums.sort()
+        if length % 2 == 0:
+            n1 = nums[length // 2-1]
+            n2 = nums[length // 2]
+            median = (n1 + n2) / 2
+        else:
+            median = nums[length // 2]
+        return median
 
-# input should be list of numbers
+    elif isinstance(nums, dict):
+        if q == 2:
+            for value, freq in nums.items():
+                if freq > 0.5:
+                    median = value
+                    return median
+        elif q == 1:
+            for value, freq in nums.items():
+                if freq > 0.25:
+                    q1 = value
+                    return q1
+        elif q == 3:
+            for value, freq in nums.items():
+                if freq > 0.75:
+                    q3 = value
+                    return q3
+
+
+# input should be list of numbers or cum freq.
 def calc_quartili(nums_copy):
     nums = nums_copy.copy()
     length = len(nums)
-    nums.sort()
-    q2 = calc_median(nums)
-    if length % 2 == 0:
-        nums_1 = nums[:length // 2]
-        q1 = calc_median(nums_1)
-        nums_3 = nums[length // 2:]
-        q3 = calc_median(nums_3)
-    else:
-        nums_1 = nums[:(length // 2)+1]
-        q1 = calc_median(nums_1)
-        nums_3 = nums[length // 2:]
-        q3 = calc_median(nums_3)
     quartili = {}
-    quartili["Q0"] = nums[0]
-    quartili["Q1"] = q1
-    quartili["Q2"] = q2
-    quartili["Q3"] = q3
-    quartili["Q4"] = nums[-1]
-    return quartili
+    if isinstance(nums, dict):
+        key_nums = list(nums.keys())
+        quartili["Q0"] = key_nums[0]
+        quartili["Q1"] = calc_median(nums, 1)
+        quartili["Q2"] = calc_median(nums, 2)
+        quartili["Q3"] = calc_median(nums, 3)
+        quartili["Q4"] = key_nums[-1]
+        return quartili
 
-# input should be list of numbers
+# input should be list abs freq.
 # return coeficciente di variazione, varianza, scarto_q_medio
 def calc_variabilita(nums_copy):
     nums = nums_copy.copy()
     media = calc_avg(nums)
     devianza = 0
-    for i in nums:
-        valore = (i - media)**2
+    lenght = sum(nums.values())
+    for value, freq in nums.items():
+        valore = ((value - media) ** 2) * freq
         devianza += valore
-    varianza = devianza / len(nums)
-    scarto_q_medio = sqrt(varianza)
+    varianza = devianza / lenght
+    scarto_q_medio = sqrt(varianza)  
     cv = (scarto_q_medio / media) * 100
     return cv, varianza, scarto_q_medio
 
-def calc_scost_mediana(nums_copy):
+# input should be list abs freq.
+def calc_scost_mediana_perc(nums_copy):
     nums = nums_copy.copy()
-    mediana = calc_median(nums)
-    sum = 0
-    for i in nums:
-        valore = abs(i - mediana)
-        sum += i
-    scost_mediana = sum / len(nums)
-    return scost_mediana
+    mediana = calc_median(nums, 1)
+    summ = 0
+    length = sum(nums.values())
+    media = calc_avg(nums)
+    for value, freq in nums.items():
+        valore = abs(value - mediana)*freq
+        summ += valore
+    scost_mediana = summ / length
+    scost_mediana_perc = (scost_mediana * 100) / media
+    return scost_mediana_perc
 
 # return indice_fisher
 def calc_asimmetria(nums_copy):
     nums = nums_copy.copy()
     media = calc_avg(nums)
-    sum = 0
-    for i in nums:
-        valore = (i - media)**3
-        sum += valore
-    indice_asimm = sum / len(nums)
+    summ = 0
+    lenght = sum(nums.values())
+    for value, freq in nums.items():
+        valore = ((value - media)**3)*freq
+        summ += valore
+    indice_asimm = summ / lenght
     result = calc_variabilita(nums_copy)
     scarto_q_medio = result[2]
     indice_fisher = indice_asimm / (scarto_q_medio**3)
     return indice_fisher
 
+# input should be nums or abs freq.
+def give_overview_box_plot(nums_copy):
+    if isinstance(nums_copy, list):
+        nums_copy.sort()
+        nums_abs_freq = calc_abs_freq(nums_copy)
+    nums_rel_freq = calc_rel_freq(nums_abs_freq)
+    nums_cum_freq = calc_cum_freq(nums_rel_freq)
+    media = calc_avg(nums_abs_freq)
+    moda = calc_moda(nums_abs_freq)
+    quartili = calc_quartili(nums_cum_freq)
+    coeff_variazione, varianza, scarto_q_medio = calc_variabilita(nums_abs_freq)
+    scost_mediana_perc = calc_scost_mediana_perc(nums_abs_freq)
+    index_fisher = calc_asimmetria(nums_abs_freq)
+    li = quartili["Q1"] - (1.5*(quartili["Q3"]-quartili["Q1"]))
+    ls = quartili["Q3"] + (1.5*(quartili["Q3"]-quartili["Q1"]))
+    print("———————Frequenze———————")
+    print(f"Assoluta: {nums_abs_freq}")
+    print(f"\nRelativa: {nums_rel_freq}")
+    print(f"\nCumulata: {nums_cum_freq}")
+    print("\n———————Medie———————")
+    print(f"Media: {round(media,3)}")
+    print(f"Quartili: {quartili}")
+    print(f"Moda: {moda}")
+    print("\n———————Box Plot———————")
+    print(f"Li: {round(li,3)}")
+    print(f"Ls: {round(ls,3)}")
+    print("\n———————Varianza———————")
+    print(f"Coeff. Var.: {round(coeff_variazione, 3)}%")
+    print(f"Varianza: {round(varianza, 3)}")
+    print(f"Scarto Q. M.: {round(scarto_q_medio, 3)}")
+    print(f"Scost. Mediana: {round(scost_mediana_perc, 3)}%")
+    print("\n———————Simmetria———————")
+    print(f"Indice Fisher: {round(index_fisher,3)}")
 
 
-numbers = [26, 28, 29, 29, 30, 32, 32, 33, 35, 36, 36, 37,37,37,39,39,42,42,43,47,48]
+#numbers = [26, 28, 29, 29, 30, 32, 32, 33, 35, 36, 36, 37,37,37,39,39,42,42,43,47,48]
 #numbers = [0,2,6,8,11,15,17,17,18,18,19,19,20,23,23,23,24,26,27,27,28]
+numbers = [0.4, 0.9, 3.8, 0.4, 2.2, 1.8, 0.4, 3.8, 1.4, 1, 0.7, 4.3, 1.3, 0.1, 0.3, 0.5, 1, 4.1, 1.6, 2.7, 0.6 ]
 
-cv, varianza, scarto_q_medio = calc_variabilita(numbers)
-
-fisher = calc_asimmetria(numbers)
-
-media = calc_avg(numbers)
-mediana = calc_median(numbers)
-abs_freq = calc_abs_freq(numbers)
-moda = calc_moda(abs_freq)
-
-quart = calc_quartili(numbers)
-
-print("————————————————————————")
-print(quart)
-print()
-
-print(f"Mediana: {mediana}\nMedia: {media}\nModa: {moda}\n")
-
-print(f"Coeff. var.: {cv}\nVarianza: {varianza}\nScarto Q Medio: {scarto_q_medio}")
-print(f"Indice di Fisher= {fisher}")
-print("————————————————————————")
+give_overview_box_plot(numbers)
