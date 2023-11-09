e5820ac9bb23206922297832f7b7a343e2984918
Matteo Ladislai <matteo.ladislai@hotmail.com>
2023-11-09 16:04:41 +0100

    idk

--- car.py
+++ car.py
@@ -1,27 +1,4 @@
-from math import sqrt
+import numpy as np
 
-class Car():
-
-    def __init__(self, hp, weight):
-        self.hp = hp
-        self.speed = 0
-        self.weight = weight
-
-    def accelerate(self, time):
-        for i in range(0, time):
-            try:
-                acc = sqrt(-(self.weight/self.hp)*(i**2) + (self.hp/2)*i + (self.hp))-(self.speed+1)/5.9
-            except ValueError:
-                acc = 0.3
-            round(acc, 2)
-            self.speed = self.speed + (acc*i)
-            print(self.speed)
-    
-    def braking(self, time):
-        for _ in range(0, time):
-            self.speed = self.speed - 4
-            print(self.speed)
-
-mustang = Car(1001, 2000)
-mustang.accelerate(5)
-mustang.braking(3)
+list1 = [i for i in range(0,100)]
+list2
